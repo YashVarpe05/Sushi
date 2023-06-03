d6bf247814ca9a4e5102654bee459dd799e66cac
yash <yashvarpe2005@gmail.com>
2023-06-03 17:59:26 +0530

    about and popular card section add with animation

--- Sushi/js/script.js
+++ Sushi/js/script.js
@@ -4,6 +4,9 @@ import check from "../assets/sushi-12.png";
 import check from "../assets/sushi-11.png";
 import check from "../assets/sushi-10.png";
 
+import { AOS } from "aos";
+import "aos/dist/aos.css";
+
 AOS.init({
 	duration: 1000,
 	offset: 100,
