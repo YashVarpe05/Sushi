d39e22edd00a8a412d116e3544012c7ef2e25d41
yash <yashvarpe2005@gmail.com>
2023-06-03 14:28:46 +0530

    navbar done

---
+++ Sushi/js/script.js
@@ -0,0 +1,19 @@
+import check from "../assets/check.svg";
+import check from "../assets/star.svg";
+import check from "../assets/sushi-12.png";
+import check from "../assets/sushi-11.png";
+import check from "../assets/sushi-10.png";
+
+AOS.init({
+	duration: 1000,
+	offset: 100,
+});
+
+const trandingsushi = [
+	"Make Sushi",
+	"Nigiri Sushi ",
+	"Oshizushi",
+	"Temaki Sushi",
+	"Uramaki Sushi",
+	"Inari Sushi",
+];
