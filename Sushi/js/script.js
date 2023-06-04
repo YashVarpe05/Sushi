70c6692c9da28b117703a26818558eab69e6e768
yash <yashvarpe2005@gmail.com>
2023-06-04 17:24:51 +0530

    add AOS fade animation to header section

--- Sushi/js/script.js
+++ Sushi/js/script.js
@@ -1,17 +1,20 @@
-import check from "../assets/check.svg";
-import check from "../assets/star.svg";
-import check from "../assets/sushi-12.png";
-import check from "../assets/sushi-11.png";
-import check from "../assets/sushi-10.png";
+// import check from "../assets/check.svg";
+// import check from "../assets/star.svg";
+// import check from "../assets/sushi-12.png";
+// import check from "../assets/sushi-11.png";
+// import check from "../assets/sushi-10.png";
 
-import { AOS } from "aos";
-import "aos/dist/aos.css";
-
-AOS.init({
-	duration: 1000,
-	offset: 100,
-});
-
+// import { AOS } from "aos";
+// import "aos/dist/aos.css";
+// init AOS animation
+// AOS.init({
+// 	duration: 1000,
+// 	offset: 100,
+// });
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles // .. AOS.init();
+AOS.init({ duration: 1000, offset: 100 });
+// aos.init({ duration: 1000, offset: 100 });
 const trandingsushi = [
 	"Make Sushi",
 	"Nigiri Sushi ",
